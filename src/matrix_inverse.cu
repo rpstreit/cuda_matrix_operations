203265265a5fd9aea445681581caae73e42e6dcb
phyllisang96 <phyllisang@utexas.edu>
2018-11-24 12:11:45 -0600

    Fixed so it does not return identity but has issues with large matrix

--- src/matrix_inverse.cu
+++ src/matrix_inverse.cu
@@ -49,15 +49,15 @@ Matrix* GJE_inverse(Matrix* matrix){
 
     while(j < size){        
         //prevent divide by 0
-        pivot<<<1, size>>>(j,j, flat_matrix, matrix->GetNumCols());
+        pivot<<<1, size*2>>>(j,j, flat_matrix, matrix->GetNumCols());
         cudaDeviceSynchronize();
 
         //row reduction
-        fixRow<<<1, size>>>(flat_matrix, matrix->GetNumCols(), j);
+        fixRow<<<1, size*2>>>(flat_matrix, matrix->GetNumCols(), j);
         cudaDeviceSynchronize();
 
         //clear column
-        fixCol<<<size, size>>>(flat_matrix, matrix->GetNumCols(), j);
+        fixCol<<<size*2, size*2>>>(flat_matrix, matrix->GetNumCols(), j);
         cudaDeviceSynchronize();
         j++;
     }
